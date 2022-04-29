d4831b7a862611c5edd47c25cf89c02b8e1718d1
Vignesh Thangaraj <Vignesh.Thangaraj@Dellteam.com>
2022-04-29 16:49:03 +0530

    commit for test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css';
+import React from "react";
+// import ReactDOM from 'react-dom';
+// import { createBrowserHistory } from "history";
+import MicroFrontend from "./MicroFrontend";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+// const defaultHistory = createBrowserHistory();
+
+const{
+  REACT_APP_CUSTOMERS_HOST: customersHost,
+  REACT_APP_CLIENTS_HOST: clientHost
+}=process.env;
+
+
+function Customers({ history }) {
+  return <MicroFrontend history={history} host={customersHost} name="Customers" />;
+}
+
+function Clients({ history }) {
+  return <MicroFrontend history={history} host={clientHost} name="Clients" />;
+}
+// function CustomerBtn({ history }) {
+//   return <MicroFrontend history={history} host={customersHost} name="Customers" />;
+// }
+
+function Home() {
+return (
+  <div className="home">
+    <div className="content">
+      <div className="Customer">
+          <Customers/>
+      </div>
+      <div>
+         <Clients/>
+      </div>
+    </div>
+  </div>
+);
+}
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          {/* <Route exact path="/cutomer" component={CustomerBtn} /> */}
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
